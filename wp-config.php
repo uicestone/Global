a9ff731d29711e68e02ac043b1eed5a031dba84f
陆秋石 <uicestone@gmail.com>
2013-08-21 15:38:19 +0800

    Wordpress 3.6

--- wp-config.php
+++ wp-config.php
@@ -18,10 +18,10 @@
 define('DB_NAME', 'global');
 
 /** MySQL 数据库用户名 */
-define('DB_USER', 'root');
+define('DB_USER', 'global');
 
 /** MySQL 数据库密码 */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '5RfzvJq5ZNrxubJp');
 
 /** MySQL 主机 */
 define('DB_HOST', '127.0.0.1');
